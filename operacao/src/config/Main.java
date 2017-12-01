13ee5df91e99b966313f79655ffd3faafb47e83a
Hugo-Coutinho <hugocoutinho2011@gmail.com>
2017-12-01 11:38:55 -0200

    Update Main.java

--- operacao/src/config/Main.java
+++ operacao/src/config/Main.java
@@ -12,38 +12,3 @@ public class Main {
 	}
 }
 
-
-//alter table Endereco 
-//drop 
-//foreign key FK_55kw5rceticbj76wi4u8kaooy
-//
-//drop table if exists Endereco
-//
-//drop table if exists usuario
-//
-//create table Endereco (
-//idEndereco integer not null auto_increment,
-//bairro varchar(255),
-//cep varchar(255),
-//cidade varchar(255),
-//estado varchar(255),
-//logradouro varchar(60),
-//id_usuario integer,
-//primary key (idEndereco)
-//)
-//
-//create table usuario (
-//idUsuario integer not null auto_increment,
-//email varchar(50),
-//nome varchar(50),
-//permissao  enum ('USUARIO','ADMINISTRADOR'),
-//senha varchar(50),
-//sexo  enum ('M','F'),
-//primary key (idUsuario)
-//)
-//
-//alter table Endereco 
-//add constraint FK_55kw5rceticbj76wi4u8kaooy 
-//foreign key (id_usuario) 
-//references usuario (idUsuario)
-//Tabelas Geradas com Sucesso !!!
